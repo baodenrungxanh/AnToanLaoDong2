5ff436ba8ae2bf06932b2fab6444d3a567839ad0
MayTinh <giang.was@gmail.com>
2019-08-21 17:56:26 +0700

    hoàn tất nhập và test ngẫu nhiên theo câu hỏi

---
+++ NhapCauHoi/EnterQuestionWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NhapCauHoi
+{
+    /// <summary>
+    /// Interaction logic for EnterQuestionWindow.xaml
+    /// </summary>
+    public partial class EnterQuestionWindow : Window
+    {
+        public string QuestionSavePath { get; set; } = "./QuestionImages";
+
+        public string AnswerSavePath { get; set; } = "./AnswerImages";
+
+        public BitmapSource QuestionBitmapSource { get; set; }
+        public BitmapSource AnswerBitmapSource { get; set; }
+
+
+        public EnterQuestionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QuestionBitmapSource = Clipboard.GetImage();
+            questionImage.Source = QuestionBitmapSource;
+        }
+
+        private void EnterAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            AnswerBitmapSource = Clipboard.GetImage();
+            answerImage.Source = AnswerBitmapSource;
+        }
+
+        private void SaveQuestionButton_Click(object sender, RoutedEventArgs e)
+        {
+            #region Kiểm tra sự tồn tại của thư mục
+            if (Directory.Exists(QuestionSavePath) == false)
+            {
+                Directory.CreateDirectory(QuestionSavePath);
+            }
+
+            if (Directory.Exists(AnswerSavePath) == false)
+            {
+                Directory.CreateDirectory(AnswerSavePath);
+            }
+            #endregion
+
+            #region Kiểm tra đã nhập thông tin đầy đủ chưa
+            if (questionImage.Source == null)
+            {
+                MessageBox.Show("Chưa nhập ảnh Câu Hỏi", "Lỗi !", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (answerImage.Source == null)
+            {
+                MessageBox.Show("Chưa nhập ảnh Câu Trả Lời", "Lỗi !", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            } 
+            #endregion
+
+            var questionName = Guid.NewGuid();
+
+            var questionImageSavePath = QuestionSavePath + "/" + questionName + ".png";
+            var answerImageSavePath = AnswerSavePath + "/" + questionName + ".png";
+
+            SaveClipboardImageToFile(QuestionBitmapSource, questionImageSavePath);
+            SaveClipboardImageToFile(AnswerBitmapSource, answerImageSavePath);
+
+            MessageBox.Show("Đã nhập câu hỏi mới", "Thông báo !", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            this.Close();
+        }
+
+        public static void SaveClipboardImageToFile(BitmapSource source, string filePath)
+        {
+            var image = source;
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(image));
+                encoder.Save(fileStream);
+            }
+        }
+    }
+}
---
+++ NhapCauHoi/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NhapCauHoi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Đường dẫn lưu câu hỏi
+        public string QuestionSavePath { get; set; } = "./QuestionImages";
+
+        public string AnswerSavePath { get; set; } = "./AnswerImages";
+
+        public int CurrentQuestion { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewQuestionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            new EnterQuestionWindow().ShowDialog();
+        }
+
+        private void ChangeQuestionButton_Click(object sender, RoutedEventArgs e)
+        {
+            var imageLinks = Directory.GetFiles(QuestionSavePath);
+            var questionCount = imageLinks.Count();
+            var random = new Random();
+
+            CurrentQuestion = random.Next(0, questionCount);
+
+            var imagePath = AppDomain.CurrentDomain.BaseDirectory +  imageLinks[CurrentQuestion];
+            questionImage.Source = new BitmapImage(new Uri(imagePath));
+            answerImage.Source = null;
+        }
+
+        private void ShowAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            var imageLinks = Directory.GetFiles(AnswerSavePath);
+            var imagePath = AppDomain.CurrentDomain.BaseDirectory + imageLinks[CurrentQuestion];
+            answerImage.Source = new BitmapImage(new Uri(imagePath));
+        }
+    }
+}
